620707c9340e66ca75c8ca15adae718c5c3ed5fb
Walaryne <hazardoustheradioactive@gmail.com>
2018-05-23 00:06:16 -0400

    Made page more XML compliant, changed color scheme, added new scrollbar margin prettfier

--- ul.topnav-js.js
+++ ul.topnav-js.js
@@ -2,3 +2,9 @@
 function activateHamburgerList() {
     document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
 }
+
+function prettify() {
+    var scrollbarsize;
+    scrollbarsize = window.innerWidth - document.body.clientWidth;
+    document.getElementById("topnav").style.width = (100 - ((scrollbarsize / window.innerWidth) * 100)) + "%";
+}
